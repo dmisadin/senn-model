6b74540fbdf2aa6bfcbb184424c9d42f46eef0b4
dmisadin <dominik.misadin@gmail.com>
2025-12-20 15:59:50 +0100

    Add checkbox to toggle us of inparams.txt

--- SENNModel/ViewModels/MainWindowViewModel.cs
+++ SENNModel/ViewModels/MainWindowViewModel.cs
@@ -7,10 +7,14 @@ namespace SENNModel.ViewModels;
 public partial class MainWindowViewModel : ObservableObject
 {
     public InputParams InputParams { get; } = new InputParams();
+    public bool ShouldUseTextFileInput { get; set; } = false;
 
     [RelayCommand]
     private void RunSimulation()
     {
-        SennRunner.Run(InputParams);
+        if (ShouldUseTextFileInput)
+            SennRunner.Run();
+        else
+            SennRunner.Run(InputParams);
     }
 }
